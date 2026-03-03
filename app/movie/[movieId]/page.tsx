02c2683ef48519490c5e8e85786c212649c3eadf
Shinyn <mattias.alm@hotmail.com>
2026-03-03 13:30:24 +0100

    Added trailer, resolved minor errors and optimized fetch

--- app/movie/[movieId]/page.tsx
+++ app/movie/[movieId]/page.tsx
@@ -1,20 +1,27 @@
 import { fetchTMDBItem } from '@/lib/tmdb';
 import Image from 'next/image';
 import { TMDB_IMAGE_BASE } from '@/lib/constants';
+import { Movie, VideoResponse } from '@/types/tmdb';
 
 export default async function Movies({ params }: { params: { movieId: string } }) {
   const { movieId } = await params;
   console.log('PARAM:', movieId);
-  const movie = await fetchTMDBItem(`/movie/${movieId}`);
-  console.log(movie);
-
-  const trailer = await fetchTMDBItem(`/movie/${movieId}/videos`);
-  console.log(trailer);
+  const movie = await fetchTMDBItem<Movie & { videos: VideoResponse }>(`/movie/${movieId}?append_to_response=videos`);
+  console.log('This is the movie', movie);
+  const officialTrailer = movie.videos.results.find(
+    (video) => video.type === 'Trailer' && video.site === 'YouTube' && video.official,
+  );
 
   return (
     <div className="h-screen">
-      <section className="bg-center bg-[url(@/public/movie-theater.png)]  bg-cover flex items-center h-full justify-center align-middle relative">
-        <div className="flex justify-center max-[1000px]:flex-col max-w-200 max-[1000px]:place-items-center max-[1000px]:px-2">
+      <section className="p-4 bg-center bg-[url(@/public/movie-theater.png)] backdrop-blur-md bg-cover flex flex-col gap-4 items-center h-full justify-center align-middle relative">
+        <iframe
+          className="w-full max-w-300 px-2 h-full"
+          src={`https://www.youtube.com/embed/${officialTrailer?.key}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <div className="flex justify-center max-[1000px]:flex-col max-w-200 z-50 max-[1000px]:place-items-center max-[1000px]:p-2">
           <Image
             src={`${TMDB_IMAGE_BASE}/${movie.poster_path}`}
             alt={`${movie.title}`}
@@ -24,7 +31,7 @@ export default async function Movies({ params }: { params: { movieId: string } }
             max-[1000px]:rounded-l-0 max-[1000px]:rounded-t-2xl
              max-[1000px]:border-b-0 max-[1000px]:rounded-b-none"
           />
-          <p
+          <div
             className="max-[1000px]:w-[clamp(20vw,400px,100%)] w-[clamp(25vw,30vw,100%)] 
             bg-white/50 border justify-between flex flex-col 
             text-black text-pretty border-l-0 rounded-r-2xl max-[1000px]:rounded-t-none max-[1000px]:rounded-b-2xl max-[1000px]:border-l font-bold 
@@ -32,15 +39,15 @@ export default async function Movies({ params }: { params: { movieId: string } }
              text-xl p-4 text-center"
           >
             <span className="text-2xl border-b-2 border-amber-800">{movie.original_title}</span>
-            <span>{movie.overview}</span>
-            <div className="flex justify-center gap-2 bg-red-500/70 rounded-md">
+            <p>{movie.overview}</p>
+            <span className="flex justify-center gap-2 bg-red-500/70 rounded-md">
               {movie.genres.map((item) => {
-                return <span key={movie.id}>{item.name}</span>;
+                console.log('This is the genres', item.name);
+                return <span key={item.id}>{item.name}</span>;
               })}
-            </div>
-
+            </span>
             <span className="place-content-end rounded-md bg-white/70">{movie.release_date}</span>
-          </p>
+          </div>
         </div>
       </section>
     </div>
