81fa3156739da03b8c0654b75a23c0d2e4f6b5fe
Shinyn <mattias.alm@hotmail.com>
2026-03-04 19:27:07 +0100

    Fixed pagination functionality and updated the UI

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -2,6 +2,7 @@
 
 import { useSearchParams, useRouter } from 'next/navigation';
 import { Movie, TMDBResponse } from '@/types/tmdb';
+import { ChevronsLeft, ChevronLeft, ChevronsRight, ChevronRight } from 'lucide-react';
 
 export default function Pagination({ page, results, total_pages, total_results }: TMDBResponse<Movie>) {
   const searchParams = useSearchParams();
@@ -10,6 +11,8 @@ export default function Pagination({ page, results, total_pages, total_results }
   console.log(
     `Pagination here, we got page ${page}, total pages ${total_pages}, results ${results.map((e) => e.title)} and total results ${total_results}`,
   );
+
+  const pages = Array.from({ length: total_pages }, (_, i) => i + 1);
 
   const currentPage = Number(searchParams.get('page') ?? 1);
   const category = searchParams.get('category') ?? 'now_playing';
@@ -23,19 +26,46 @@ export default function Pagination({ page, results, total_pages, total_results }
 
   return (
     <div>
-      <div className="flex p-2 justify-center bg-blue-950 gap-2">
-        <button onClick={() => changePage(1)} disabled={currentPage === 1}>
-          First
+      <div className="flex p-2 justify-center gap-2">
+        <button
+          className={`hover:cursor-pointer hover:bg-white hover:text-black p-2 rounded-md`}
+          onClick={() => changePage(1)}
+          disabled={currentPage === 1}
+        >
+          <ChevronsLeft />
         </button>
-        <button onClick={() => changePage(currentPage - 1)} disabled={currentPage === 1}>
-          Previous
+        <button
+          className={`hover:cursor-pointer hover:bg-white hover:text-black p-2 rounded-md`}
+          onClick={() => changePage(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          <ChevronLeft />
         </button>
-        <button>{page}</button>
-        <button onClick={() => changePage(currentPage + 1)} disabled={currentPage === total_pages}>
-          Next
+        <select
+          className={`hover:cursor-pointer p-2 rounded-md`}
+          name="page"
+          value={currentPage}
+          onChange={(e) => changePage(Number(e.target.value))}
+        >
+          {pages.map((pageNumber) => (
+            <option key={pageNumber} value={pageNumber}>
+              Page {pageNumber}
+            </option>
+          ))}
+        </select>
+        <button
+          className={`hover:cursor-pointer hover:bg-white hover:text-black p-2 rounded-md`}
+          onClick={() => changePage(currentPage + 1)}
+          disabled={currentPage === total_pages}
+        >
+          <ChevronRight />
         </button>
-        <button onClick={() => changePage(total_pages)} disabled={currentPage === total_pages}>
-          Last
+        <button
+          className={`hover:cursor-pointer hover:bg-white hover:text-black p-2 rounded-md`}
+          onClick={() => changePage(total_pages)}
+          disabled={currentPage === total_pages}
+        >
+          <ChevronsRight />
         </button>
       </div>
     </div>
