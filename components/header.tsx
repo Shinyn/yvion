ad5dbfd5a0d44a9c66f06caabdf0fd545372c7a9
Shinyn <mattias.alm@hotmail.com>
2026-03-10 04:42:47 +0100

    Added tailwind styling & removed comment

--- components/header.tsx
+++ components/header.tsx
@@ -18,26 +18,30 @@ export function Header({ data }: HeaderProps) {
   }
 
   return (
-    <header className="header">
+    <header className="flex flex-col justify-center">
       {/* Gör om denna Image till ett bildspel som loopar igenom samtliga filmer 
           (på nuvarande page) och har dom som bakgrundsbild. Ska fade'a till svart
           mot botten
       */}
       <Image
-        className=" overflow-clip "
+        className=" overflow-clip w-full"
         src={`${TMDB_IMAGE_BASE}/${data.results[0].backdrop_path}`}
         alt="background"
         width={500}
         height={250}
       ></Image>
+      <input
+        type="search"
+        placeholder="I want to watch..."
+        className=" border border-white rounded-2xl justify-self-center w-[clamp(200px,50vw,30rem)] px-4 py-[.2rem] m-4 mx-auto bg-[#1f1f1f]"
+      />
       <Pagination
         page={data.page}
         results={data.results}
         total_pages={data.total_pages}
         total_results={data.total_results}
       />
-      <input type="text" placeholder="I want to watch..." className="search" />
-      <select name="Movie Lists" value={currentCategory} onChange={handleChange}>
+      <select className="text-black bg-white" name="Movie Lists" value={currentCategory} onChange={handleChange}>
         <option value="now_playing">Now Playing</option>
         <option value="popular">Popular</option>
         <option value="top_rated">Top Rated</option>
@@ -46,7 +50,3 @@ export function Header({ data }: HeaderProps) {
     </header>
   );
 }
-
-/*
-Ska innehålla filtrering, searchbar, logo, paginering och nån typ av bakgrundsbild
-*/
