ebd68ba0b70ab2c63ad38829812afd83ca0b3f79
Shinyn <mattias.alm@hotmail.com>
2026-03-13 04:55:08 +0100

    Replaced placeholder text with something coherent

--- app/movie/page.tsx
+++ app/movie/page.tsx
@@ -1,12 +1,7 @@
 export default async function Movie() {
   return (
     <>
-      <h1>Hehehehehehehehe</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae maxime explicabo aperiam tempore quas,
-        consequatur itaque eum cum aliquam, temporibus cupiditate amet qui adipisci possimus laboriosam dolore odit
-        molestiae soluta.
-      </p>
+      <h1>What are you doing here? Something must have gone terribly wrong.. You should probably skedaddle..</h1>
     </>
   );
 }
